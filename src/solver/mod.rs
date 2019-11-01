9fb863e671edddd57dda9e38dad891513e13f406
Karl <37496339+aquarial@users.noreply.github.com>
2019-11-01 13:08:02 -0500

    Use Amazons struct to represent board state (and history)

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -2,18 +2,18 @@ pub mod board;
 
 use board::*;
 
-pub struct Solver {
+pub struct Amazons {
     board_size: u8,
-    board: Board,
-    cache: DistState,
+    boards: Vec<Board>,
+    cache: Vec<DistState>,
 }
 
 
-impl Solver {
-    pub fn new_8x8() -> Solver {
+impl Amazons {
+    pub fn new_8x8() -> Amazons {
         let board_size = 8 + 2;
 
-        let players = Vec::new();
+        let mut players = Vec::new();
         players.push(Player{ team:Team::White, pos:Pos {row:  3, col:  3} });
         players.push(Player{ team:Team::White, pos:Pos {row:  3, col:  6} });
         players.push(Player{ team:Team::Black, pos:Pos {row:  6, col:  3} });
@@ -21,10 +21,10 @@ impl Solver {
 
         let mut board = Board::new(board_size, players);
 
-        Solver {
+        Amazons {
             board_size: board_size,
-            board: board,
-            cache: DistState::new(board_size),
+            boards: vec![board],
+            cache: vec![DistState::new(board_size)],
         }
     }
 
@@ -53,6 +53,5 @@ impl Solver {
         }
         println!("Move not a player");
         return false;
-
     }
 }
