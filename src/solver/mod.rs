688b0f3d7fc9678819da8fc9472369b6fabef238
Karl <37496339+aquarial@users.noreply.github.com>
2019-11-01 12:41:00 -0500

    Change ix0/1 to 3/6

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -14,12 +14,10 @@ impl Solver {
         let board_size = 8 + 2;
 
         let players = Vec::new();
-        let ix0 = 3;
-        let ix1 = 6;
-        players.push(Player{ team:Team::White, pos:Pos {row: ix0, col: ix0} });
-        players.push(Player{ team:Team::White, pos:Pos {row: ix0, col: ix1} });
-        players.push(Player{ team:Team::Black, pos:Pos {row: ix1, col: ix0} });
-        players.push(Player{ team:Team::Black, pos:Pos {row: ix1, col: ix1} });
+        players.push(Player{ team:Team::White, pos:Pos {row:  3, col:  3} });
+        players.push(Player{ team:Team::White, pos:Pos {row:  3, col:  6} });
+        players.push(Player{ team:Team::Black, pos:Pos {row:  6, col:  3} });
+        players.push(Player{ team:Team::Black, pos:Pos {row:  6, col:  6} });
 
         let mut board = Board::new(board_size, players);
 
